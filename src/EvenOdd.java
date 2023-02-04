f87c9f60c96d27030fa216da8fbe2ff8f2185d00
Mark Owings <owingsmk@mail.uc.edu>
2023-02-04 14:20:40 -0500

    Second Commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the first number: "); //output asking for the users first number
+        int number1 = 2; //This would normally be asking the use to input thje first number for this program
+        System.out.println("Enter the second number: ");
+        int number2 = 7; //This would be the input for the second number from the user
+
+
+    }
+}
