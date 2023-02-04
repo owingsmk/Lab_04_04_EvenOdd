52b8881bbf44357a9887e2e192fbeefd40e63beb
Mark Owings <owingsmk@mail.uc.edu>
2023-02-04 14:59:57 -0500

    Fourth Commit

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,13 +2,14 @@ public class EvenOdd {
     public static void main(String[] args) {
 
         System.out.println("Enter the first number: "); //output asking for the users first number
-        int number1 = 2; //This would normally be asking the use to input the first number for this program
+        int numberA = 2; //This would normally be asking the use to input the first number for this program
 
 
-        if (number1 % 2==0); //even number divid by 2 returns 0 reminder
-            System.out.println(number1 + " is even. ");
-        else //else its odd
-            System.out.println(number1 + " Odd number. ");
+        if(numberA%2==0); //even number divid by 2 returns 0 reminder}
+            System.out.println(numberA + " is even. ");
+
+        else(numberA%2==1); //else its odd
+                System.out.println(numberA + " Odd number. ");
 
     }
 }
